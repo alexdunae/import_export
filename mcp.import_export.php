4d0f6d14afa2e3d642909750cca137cfd93693df
Alex Dunae <alex@dunae.ca>
2010-12-17 11:32:14 -0800

    Export categories

--- mcp.import_export.php
+++ mcp.import_export.php
@@ -41,10 +41,10 @@
 		{
 			$this->EE->load->model('channel_entries_model');
 
-			// TODO
-			header('Content-type: text/plain');
-			$channel_id = 16;
+			header('Content-type: text/csv');
+			$channel_id = $this->EE->input->post('channel_id', TRUE);
 
+			// channel info
 			$channel = $this->EE->channel_model->get_channel_info($channel_id)->result();
 			$channel = current($channel);
 
@@ -54,28 +54,47 @@
 			}
 
 			$fields = $this->_get_field_list($channel->field_group);
-			$field_count = count($fields);
+					
+			// categories
+			$cat_groups = array();
+			$post_categories = array();
+
+			if (empty($channel->cat_group) === FALSE) 
+			{
+				$cat_groups = $this->_get_category_groups($channel->cat_group);
+				$post_categories = $this->_get_post_categories($channel->cat_group);
+			}
+
+			// entries
+			$entries = array();
 
 			$this->EE->db->select('t.*, d.*');
 			$this->EE->db->from('channel_titles AS t, channel_data AS d');
 			$this->EE->db->where('t.channel_id', $channel_id);
 			$this->EE->db->where('t.entry_id = d.entry_id', NULL, FALSE);
 			$entries_query = $this->EE->db->get();
-			
 
-			$entries = array();
+
+
 
 			ob_clean();
 
-			// requires PHP >= 5.1.0
+			// build the CSV file; fputcsv requires PHP >= 5.1.0
 			$out = fopen('php://output', 'w');
-			fputcsv($out, array_values($fields));
-			
+
+			// build the header row, showing categories first
+			$headers = array_merge(array_values($cat_groups), $fields);	
+			fputcsv($out, $headers);
+			$field_count = count($headers);	
 			
 			foreach($entries_query->result_array() as $entry)
 			{
-				// TODO: there must be a PHP built-in that does this
 				$row = array();
+
+				foreach($cat_groups as $group_id => $group_slug)
+				{
+					$row[] = isset($post_categories[$entry['entry_id']]) ? @$post_categories[$entry['entry_id']][$group_id] : '';
+				}
 
 				foreach($fields as $db => $label) 
 				{
@@ -113,5 +132,67 @@
 
 			return $fields;
 		}
+
+		/**
+		 * Returns an array of group_id => group_name for one or more category groups (e.g. 1|2|3).
+		 */
+		function _get_category_groups($group_ids)
+		{
+			$this->EE->load->model('category_model');
+
+			$group_ids = explode('|', $group_ids);
+
+			$this->EE->db->select("group_id, group_name");
+			$this->EE->db->from("category_groups");
+			$this->EE->db->where_in("group_id", $group_ids);
+
+			$groups_query = $this->EE->db->get();
+			
+			$groups = array();
+
+			foreach($groups_query->result() as $group)
+			{
+				$groups[$group->group_id] = $this->_cat_group_slug($group->group_name);
+			}
+
+			return $groups;
+		}
+
+		function _get_post_categories($group_ids)
+		{
+			if ($group_ids === '')
+			{				
+				return array();
+			}
+
+			$group_ids = explode('|', $group_ids);
+			
+			$sql = sprintf(
+				"SELECT cp.entry_id, c.group_id, GROUP_CONCAT(c.cat_url_title) AS cat_url_titles 
+				 FROM exp_categories AS c LEFT JOIN exp_category_posts AS cp ON cp.cat_id = c.cat_id
+				 WHERE c.group_id IN (%s) GROUP BY cp.entry_id, c.group_id;", 
+				implode(',', $group_ids));
+			
+			$q = $this->EE->db->query($sql);
+			
+			$post_categories = array();
+			foreach($q->result_array() as $row)
+			{
+
+				if (isset($post_categories[$row['entry_id']]) === FALSE)
+				{
+					$post_categories[$row['entry_id']] = array();
+				}
+				$post_categories[$row['entry_id']][$row['group_id']] = $row['cat_url_titles'];
+			}
+			
+			return $post_categories;
+		}
+		
+		function _cat_group_slug($raw)
+		{
+			return 'cat_' . preg_replace('/\W/', '_', strtolower($raw));
+		}
+
 
 	}
